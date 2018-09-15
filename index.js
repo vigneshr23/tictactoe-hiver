2297b29fa6f4511f7461d13b2d4d5034c4a74b00
Vignesh R <vrs23.92@gmail.com>
2018-09-15 13:36:25 +0530

    Logic added
    Game logic added for bot move and Draw scenario

---
+++ index.js
@@ -0,0 +1,131 @@
+/**
+ * This program is a boliler plate code for the famous tic tac toe game
+ * Here box represents one placeholder for either X or a 0
+ * We have a 2D array to represent the arrangement of X or O is a grid
+ * 0 -> empty box
+ * 1 -> box with X
+ * 2 -> box with O
+ * 
+ * Below are the tasks which needs to be completed
+ * Imagine you are playing with Computer so every alternate move should be by Computer
+ * X -> player
+ * O -> Computer
+ * 
+ * Winner has to be decided and has to be flashed
+ * 
+ * Extra points will be given for the Creativity
+ * 
+ * Use of Google is not encouraged
+ * 
+ */
+const grid = [];
+const GRID_LENGTH = 3;
+let turn = 'X';
+
+function initializeGrid() {
+    for (let colIdx = 0;colIdx < GRID_LENGTH; colIdx++) {
+        const tempArray = [];
+        for (let rowidx = 0; rowidx < GRID_LENGTH;rowidx++) {
+            tempArray.push(0);
+        }
+        grid.push(tempArray);
+    }
+}
+
+function getRowBoxes(colIdx) {
+    let rowDivs = '';
+    
+    for(let rowIdx=0; rowIdx < GRID_LENGTH ; rowIdx++ ) {
+        let additionalClass = 'darkBackground';
+        let content = '';
+        const sum = colIdx + rowIdx;
+        if (sum%2 === 0) {
+            additionalClass = 'lightBackground'
+        }
+        const gridValue = grid[colIdx][rowIdx];
+        if(gridValue === 1) {
+            content = '<span class="cross">X</span>';
+        }
+        else if (gridValue === 2) {
+            content = '<span class="circle">O</span>';
+        }
+        rowDivs = rowDivs + '<div colIdx="'+ colIdx +'" rowIdx="' + rowIdx + '" class="box ' +
+            additionalClass + '">' + content + '</div>';
+    }
+    return rowDivs;
+}
+
+function getColumns() {
+    let columnDivs = '';
+    for(let colIdx=0; colIdx < GRID_LENGTH; colIdx++) {
+        let coldiv = getRowBoxes(colIdx);
+        coldiv = '<div class="rowStyle">' + coldiv + '</div>';
+        columnDivs = columnDivs + coldiv;
+    }
+    return columnDivs;
+}
+
+function renderMainGrid() {
+    const parent = document.getElementById("grid");
+    const columnDivs = getColumns();
+    parent.innerHTML = '<div class="columnsStyle">' + columnDivs + '</div>';
+}
+
+function onBoxClick() {
+    if(this.children.length > 0) {
+        return false;
+    }
+    var rowIdx = this.getAttribute("rowIdx");
+    var colIdx = this.getAttribute("colIdx");
+    let newValue = 1;
+    grid[colIdx][rowIdx] = newValue;
+    renderMainGrid();
+    addClickHandlers();
+    computerMakesMove();
+}
+
+function computerMakesMove() {
+    console.log("computer move");
+    var emptyBoxes = [], aiSelection = "",
+        boxes = document.getElementsByClassName("box");
+    for (var idx = 0; idx < boxes.length; idx++) {
+        if(boxes[idx].children.length < 1) {
+            emptyBoxes.push(boxes[idx]);
+        }
+    }
+    if(emptyBoxes.length > 0) {
+        aiSelection = emptyBoxes[Math.ceil(Math.random()*emptyBoxes.length-1)];
+        var rowIdx = aiSelection.getAttribute("rowIdx");
+        var colIdx = aiSelection.getAttribute("colIdx");
+        let aiValue = 2;
+        grid[colIdx][rowIdx] = aiValue;
+        renderMainGrid();
+        addClickHandlers();
+    } else {
+        gameStatusDraw();
+    }
+}
+
+function endGame(msg) {
+    document.getElementById("endgameOverlay").classList.toggle("show");
+    document.getElementsByClassName("gameStatus")[0].classList.toggle("show");
+    if(msg) {
+        document.getElementsByClassName("gameEndMsg")[0].innerHTML = msg;
+    }
+}
+
+function gameStatusDraw() {
+    console.log("Game DRAW!");
+    endGame("Game Ends in a DRAW");
+}
+
+function addClickHandlers() {
+    var boxes = document.getElementsByClassName("box");
+    for (var idx = 0; idx < boxes.length; idx++) {
+        boxes[idx].addEventListener('click', onBoxClick, false);
+    }
+}
+
+initializeGrid();
+renderMainGrid();
+addClickHandlers();
